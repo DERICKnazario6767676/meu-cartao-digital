506fce9883387c8a8e05542c1825fd382e9c031d
DERICKnazario6767676 <4553573082@estudante.sed.sc.gov.br>
2026-06-01 09:42:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem(redeSocial) {
+    console.log("Redirecionando para: " + redeSocial);
+
+    alert(
+        "Você está sendo redirecionado para o meu " +
+        redeSocial +
+        "!"
+    );
+}
